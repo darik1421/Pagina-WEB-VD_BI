efac26b5a15476dd299082d7f7abc5736bc4e1a9
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2024-06-25 10:11:36 -0600

    Se modifico el margen del dashboard y header

--- frontend-vd/src/pages/BI.js
+++ frontend-vd/src/pages/BI.js
@@ -13,84 +13,15 @@ function BI({ rol }) {
 
     return (
         <div>
-           <Header rol={rol} style={{ paddingTop: '4rem' }} />
+           <Header rol={rol}/>
 
-            <Container className="mt-4">
+            <Container className="margen-contenedor">
                 <Row className="g-3">
-                    <Col xs="12">
+                    <Col sm="12" md="12" lg="12">
                         <Card>
                             <Card.Body>
-                                <Card.Title>Estado del almacén</Card.Title>
+                                <Card.Title>Dashboard</Card.Title>
                                 <div className="iframe-container mt-4"> {/* Agregamos la clase mt-4 para el margen */}
-                                    <iframe
-                                        title="REPORTE KARDEX"
-                                        src="https://app.powerbi.com/view?r=eyJrIjoiMzM0Yjk0Y2UtM2IwMy00ZTRjLTljNWUtYzNjNWQ0YmM4ZDhlIiwidCI6ImU0NzY0NmZlLWRhMjctNDUxOC04NDM2LTVmOGIxNThiYTEyNyIsImMiOjR9"
-                                        frameBorder="0"
-                                        allowFullScreen
-                                    ></iframe>
-                                </div>
-                                <Button className="mt-3" onClick={imprimirEstadisticas}>
-                                    Generar reporte con imagen
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-
-            <Container className="mt-4">
-                <Row className="g-3">
-                    <Col xs="12">
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Estado del almacén</Card.Title>
-                                <div className="iframe-container">
-                                    <iframe
-                                        title="REPORTE KARDEX"
-                                        src="https://app.powerbi.com/view?r=eyJrIjoiMzM0Yjk0Y2UtM2IwMy00ZTRjLTljNWUtYzNjNWQ0YmM4ZDhlIiwidCI6ImU0NzY0NmZlLWRhMjctNDUxOC04NDM2LTVmOGIxNThiYTEyNyIsImMiOjR9"
-                                        frameBorder="0"
-                                        allowFullScreen
-                                    ></iframe>
-                                </div>
-                                <Button className="mt-3" onClick={imprimirEstadisticas}>
-                                    Generar reporte con imagen
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-
-            <Container className="mt-4">
-                <Row className="g-3">
-                    <Col xs="12">
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Estado del almacén</Card.Title>
-                                <div className="iframe-container">
-                                    <iframe
-                                        title="REPORTE KARDEX"
-                                        src="https://app.powerbi.com/view?r=eyJrIjoiMzM0Yjk0Y2UtM2IwMy00ZTRjLTljNWUtYzNjNWQ0YmM4ZDhlIiwidCI6ImU0NzY0NmZlLWRhMjctNDUxOC04NDM2LTVmOGIxNThiYTEyNyIsImMiOjR9"
-                                        frameBorder="0"
-                                        allowFullScreen
-                                    ></iframe>
-                                </div>
-                                <Button className="mt-3" onClick={imprimirEstadisticas}>
-                                    Generar reporte con imagen
-                                </Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-
-            <Container className="mt-4 mb-4">
-                <Row className="g-3">
-                    <Col xs="12">
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Estado del almacén</Card.Title>
-                                <div className="iframe-container">
                                     <iframe
                                         title="REPORTE KARDEX"
                                         src="https://app.powerbi.com/view?r=eyJrIjoiMzM0Yjk0Y2UtM2IwMy00ZTRjLTljNWUtYzNjNWQ0YmM4ZDhlIiwidCI6ImU0NzY0NmZlLWRhMjctNDUxOC04NDM2LTVmOGIxNThiYTEyNyIsImMiOjR9"
